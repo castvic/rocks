0225cf89fb3a7e5338998282e85d0c65af71a896
castvic <castvic@gmail.com>
2021-03-20 19:15:23 -0700

    Initial
    Initial Creation

---
+++ rocks.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify, make_response
+
+example_1='''. .
+. .
+ :T.
+. .
+ .'''
+
+print(example_1)
+col1 = []
+col2 = []
+col3 = []
+col4 = []
+rocks = [col1, col2, col3, col4
+
+app = Flask(__name__)
+
+
+#Handle Get Requests
+@app.route('/',methods =['GET'])
+def hello():
+    return "Hello world!"
+
+
+#Handle Post Requests
+@app.route('/',methods = ['POST'])
+def initialize():
+
+
+#Handle Put Requests
+@app.route('/',methods = ['PUT'])
+
+
+
+
+#Handle Delete Requests
+@app.route('/',methods = ['DELETE'])
+
+
+if __name__ == '__main__':
+    app.run()
